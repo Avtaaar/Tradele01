1f5b2d4503b7d584b76b0f90642e2be937a2e6a8
Avtaar Kalsi <avi@Mac.lan>
2025-01-17 12:24:43 -0800

    Full PAGE

--- tradele01/app/page.tsx
+++ tradele01/app/page.tsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>My Image in Next.js (App Router)</h1>
-      <Image src="/load.jpg" alt="My Image" width={500} height={300} />
+    <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+      <Image
+        src="/load.jpg"
+        alt="Full-screen image"
+        layout="fill"
+        objectFit="cover"
+      />
     </div>
   );
 }
